061c1b2c78372e2556d41629c30788f40b4d489b
Vinicius Mattera <vinimatteraz@gmail.com>
2025-03-25 23:49:06 -0300

    feature: add zipService class to compact files in zip

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from services.pdfService import PdfService
 from services.csvService import CsvService
+from services.zipService import ZipService
 
 pdfService = PdfService()
 
 def main():
     data = pdfService.extractData("./data/Anexo I.pdf")
     suce, file = CsvService.saveToCsv(data)
+    if suce:
+        if ZipService.compactCsv(file): 
+            print("O arquivo foi zipado com sucesso!")
+    else:
+        print("Failed saving tables in csv file")
 
 if __name__ == "__main__":
     main()
--- src/services/zipService.py
+++ src/services/zipService.py
@@ -1,2 +1,13 @@
+import zipfile
+import os
+
 class ZipService:
-    pass
+    def compactCsv(file: str) -> bool:
+        outDir = os.path.dirname(file)
+        zipPath = os.path.join(outDir, "Teste_Vinicius_Mattera.zip")
+        try:
+            with zipfile.ZipFile(zipPath, "w") as fileZip:
+                fileZip.write(file, arcname="Output.csv")
+            return True
+        except Exception as e:
+            raise e
