625ef7a80d814a13dd9770dede0ce3ee525a2b25
Vinicius Mattera <vinimatteraz@gmail.com>
2025-03-25 23:48:23 -0300

    feature: add csvService class to manipulate csv files

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from services.pdfService import PdfService
+from services.csvService import CsvService
 
 pdfService = PdfService()
 
 def main():
     data = pdfService.extractData("./data/Anexo I.pdf")
+    suce, file = CsvService.saveToCsv(data)
 
 if __name__ == "__main__":
     main()
--- src/services/csvService.py
+++ src/services/csvService.py
@@ -1,2 +1,17 @@
+import csv
+import os
+
 class CsvService:
-    pass
+    def saveToCsv(tables: list[list[str]]) -> tuple[bool, str]:
+        try:
+            
+            baseDir = os.path.dirname(os.path.abspath(__file__))
+            filePath = os.path.join(baseDir, "../../data", "output.csv")
+            with open(filePath, mode="w+", newline="", encoding="utf-8-sig") as file:
+                writer = csv.writer(file, delimiter=";")
+                for table in tables:
+                    for row in table:
+                        writer.writerow(row)
+            return True, filePath 
+        except Exception as e:
+            raise e     
